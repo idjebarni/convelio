ddd45169baae43363d1e13a08ca73a7b62b95a53
imad-dh <74598949+imad-dh@users.noreply.github.com>
2022-08-23 21:56:07 +0200

    feat(UI):
    - add redirection on navbar
    - change color for even row

--- apps/convelio/src/app/modules/user-list/user-list.module.ts
+++ apps/convelio/src/app/modules/user-list/user-list.module.ts
@@ -4,10 +4,8 @@ import {CommonModule} from '@angular/common';
 import {UserListRoutingModule} from './user-list-routing.module';
 import {UserListComponent} from './user-list.component';
 import {UserDetailsComponent} from './components/user-details/user-details.component';
-import {MatCardModule} from '@angular/material/card';
 import {HttpClientModule} from '@angular/common/http';
 import {UserService} from './service/user.service';
-import {MatIconModule} from "@angular/material/icon";
 import {MatTableModule} from "@angular/material/table";
 import {MatSortModule} from "@angular/material/sort";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
@@ -17,9 +15,7 @@ import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
   imports: [
     CommonModule,
     UserListRoutingModule,
-    MatCardModule,
     HttpClientModule,
-    MatIconModule,
     MatTableModule,
     MatSortModule,
     MatProgressSpinnerModule,
