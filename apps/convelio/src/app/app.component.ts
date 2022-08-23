5b2744e3f80708e99b43204885092a04a98bff43
imad-dh <74598949+imad-dh@users.noreply.github.com>
2022-08-23 13:03:45 +0200

    first commit

---
+++ apps/convelio/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'convelio-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'convelio-app';
+}
