b0ed58b37ed3df2691f70fd5c18a17eb7d8ffb5c
imad-dh <74598949+imad-dh@users.noreply.github.com>
2022-08-23 19:20:01 +0200

    feat: add toolbar for application

--- apps/convelio/src/app/app.module.ts
+++ apps/convelio/src/app/app.module.ts
@@ -4,10 +4,11 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule],
   providers: [],
   bootstrap: [AppComponent],
 })
