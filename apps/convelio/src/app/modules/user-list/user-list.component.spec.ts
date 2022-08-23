e7c9a287bcc528bd62d967fa5785b9a14e164577
imad-dh <74598949+imad-dh@users.noreply.github.com>
2022-08-23 21:56:31 +0200

    feat: unit test for user service

--- apps/convelio/src/app/modules/user-list/user-list.component.spec.ts
+++ apps/convelio/src/app/modules/user-list/user-list.component.spec.ts
@@ -1,16 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { UserListComponent } from './user-list.component';
+import {UserListComponent} from './user-list.component';
+import {HttpClientModule} from "@angular/common/http";
+import {UserService} from "./service/user.service";
+import {MatTableModule} from "@angular/material/table";
+import {MatSortModule} from "@angular/material/sort";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
   let fixture: ComponentFixture<UserListComponent>;
+  let service: UserService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [UserListComponent],
+      imports: [HttpClientModule, MatTableModule, MatSortModule, MatProgressSpinnerModule],
+      providers: [UserService]
     }).compileComponents();
-
+    service = TestBed.inject(UserService);
     fixture = TestBed.createComponent(UserListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -18,5 +26,22 @@ describe('UserListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('service should contain first user with Bret as username', () => {
+    expect(service).toBeTruthy();
+    service.getUsers().subscribe((result) => {
+      expect(result[0].username).toBe('Bret');
+    });
+  });
+
+  it('first user should be displayed correctly', () => {
+    const fixture = TestBed.createComponent(UserListComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    console.log(compiled)
+    /*    expect(compiled.querySelector('table-row')?.textContent).toContain(
+          'Welcome convelio-app'
+        );*/
   });
 });
