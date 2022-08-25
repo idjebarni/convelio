0226845191116ade44b4ce252b860b0c16398c40
imad-dh <74598949+imad-dh@users.noreply.github.com>
2022-08-25 20:49:39 +0200

    refacto:
    - UI for user list table
    - table sorting for city & company name
    - add border box on global styles

---
+++ apps/convelio/src/app/modules/user-list/models/user-data.model.ts
@@ -0,0 +1,7 @@
+export interface UserData {
+  id: number;
+  name: string;
+  username: string;
+  city: string;
+  company: string;
+}
--- apps/convelio/src/app/modules/user-list/user-list.component.ts
+++ apps/convelio/src/app/modules/user-list/user-list.component.ts
@@ -1,6 +1,6 @@
-import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-import { Observable, Subject } from 'rxjs';
+import { filter, Observable, Subject, takeUntil } from 'rxjs';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { UserService } from './service/user.service';
@@ -8,18 +8,19 @@ import { User } from './models/user.model';
 import { getUsers } from './store/user-list.actions';
 import { MatSort } from '@angular/material/sort';
 import { UserListState } from './store/user-list.reducer';
+import { UserData } from './models/user-data.model';
 
 @Component({
   selector: 'convelio-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss'],
 })
-export class UserListComponent implements OnInit, AfterViewInit, OnDestroy {
+export class UserListComponent implements OnInit, OnDestroy {
   @ViewChild(MatSort) sort: MatSort;
 
   destroy$ = new Subject();
   displayedColumns: string[] = ['name', 'username', 'city', 'company'];
-  dataSource: MatTableDataSource<User>;
+  dataSource: MatTableDataSource<UserData>;
   isLoading = false;
   users: User[];
   users$: Observable<User[]> = this.store.select('users');
@@ -44,12 +45,6 @@ export class UserListComponent implements OnInit, AfterViewInit, OnDestroy {
     this.destroy$.complete();
   }
 
-  ngAfterViewInit() {
-    if (this.dataSource) {
-      this.dataSource.sort = this.sort;
-    }
-  }
-
   sortData() {
     this.dataSource.sort = this.sort;
   }
@@ -58,16 +53,26 @@ export class UserListComponent implements OnInit, AfterViewInit, OnDestroy {
     this.isLoading = true;
     this.store.dispatch(getUsers());
 
-    this.users$.subscribe({
-      next: (users) => {
-        if (users.length > 0) {
-          this.users = users;
-          this.dataSource = new MatTableDataSource<User>(users);
+    this.users$
+      .pipe(
+        takeUntil(this.destroy$),
+        filter((users) => !!users && users.length > 0),
+      )
+      .subscribe({
+        next: (users) => {
+          const formattedUserArray = users.map((user) => ({
+            id: user.id,
+            name: user.name,
+            username: user.username,
+            city: user.address.city,
+            company: user.company.name,
+          }));
+
+          this.dataSource = new MatTableDataSource<UserData>(formattedUserArray);
           this.dataSource.sort = this.sort;
           this.isLoading = false;
-        }
-      },
-      error: () => (this.isLoading = false),
-    });
+        },
+        error: () => (this.isLoading = false),
+      });
   }
 }
