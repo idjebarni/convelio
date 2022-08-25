2a673b1acef921295e8eb9482916706639fbe2f7
imad-dh <74598949+imad-dh@users.noreply.github.com>
2022-08-25 21:18:59 +0200

    update:
    - add header for details page
    - enable & fix some unit tests

--- apps/convelio/src/app/app.component.spec.ts
+++ apps/convelio/src/app/app.component.spec.ts
@@ -18,6 +18,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome convelio-app');
+    expect(compiled.querySelector('span')?.textContent).toContain('Convelio - Angular Technical Test');
   });
 });
--- apps/convelio/src/app/modules/user-list/components/user-details/user-details.component.spec.ts
+++ apps/convelio/src/app/modules/user-list/components/user-details/user-details.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserDetailsComponent } from './user-details.component';
+import { EffectsModule } from '@ngrx/effects';
+import { UserListEffects } from '../../store/user-list.effects';
+import { StoreModule } from '@ngrx/store';
+import { userListReducer } from '../../store/user-list.reducer';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('UserDetailsComponent', () => {
   let component: UserDetailsComponent;
@@ -9,6 +15,12 @@ describe('UserDetailsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [UserDetailsComponent],
+      imports: [
+        HttpClientModule,
+        RouterTestingModule,
+        EffectsModule.forRoot([UserListEffects]),
+        StoreModule.forRoot({ users: userListReducer }),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(UserDetailsComponent);
