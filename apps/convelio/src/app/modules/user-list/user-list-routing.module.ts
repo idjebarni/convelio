da5e1b4402bbbf704393f747173919ee44f4f997
imad-dh <74598949+imad-dh@users.noreply.github.com>
2022-08-25 20:49:54 +0200

    feat: implement & bind not found page when user id does not exists

--- apps/convelio/src/app/modules/user-list/user-list-routing.module.ts
+++ apps/convelio/src/app/modules/user-list/user-list-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserListComponent } from './user-list.component';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { NotFoundComponent } from '../shared/components/not-found/not-found.component';
 
 const routes: Routes = [
   {
     path: '',
     component: UserListComponent,
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent,
   },
   {
     path: ':userId',
--- apps/convelio/src/app/modules/user-list/user-list.module.ts
+++ apps/convelio/src/app/modules/user-list/user-list.module.ts
@@ -14,6 +14,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [UserListComponent, UserDetailsComponent],
@@ -29,6 +30,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatCardModule,
     MatButtonModule,
     MatTooltipModule,
+    SharedModule,
   ],
   providers: [UserService],
 })
