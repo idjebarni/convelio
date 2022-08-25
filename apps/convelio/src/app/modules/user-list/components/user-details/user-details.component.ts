254b10848e28e8d9537c6b56d54ddeb3863c8a8f
imad-dh <74598949+imad-dh@users.noreply.github.com>
2022-08-25 17:19:13 +0200

    feat: implement details page

--- apps/convelio/src/app/modules/user-list/components/user-details/user-details.component.ts
+++ apps/convelio/src/app/modules/user-list/components/user-details/user-details.component.ts
@@ -1,14 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { UserListState } from '../../store/user-list.reducer';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable, Subject, takeUntil } from 'rxjs';
+import { getUserById } from '../../store/user-list.selector';
+import { getUsers } from '../../store/user-list.actions';
 
 @Component({
   selector: 'convelio-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.scss'],
 })
-export class UserDetailsComponent implements OnInit {
-  constructor() {}
+export class UserDetailsComponent implements OnInit, OnDestroy {
+  selectedUser$: Observable<any>;
+  destroy$ = new Subject();
+  private actualUserId: number;
+
+  constructor(private store: Store<UserListState>, private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
-    window.scrollTo(0, 0);
+    this.store.dispatch(getUsers());
+    this.handleUrlParams();
+    this.updateCurrentUser();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.complete();
+  }
+
+  handleUrlParams() {
+    this.activatedRoute.params.pipe(takeUntil(this.destroy$)).subscribe((params: Params) => {
+      if (!isNaN(parseInt(params['userId']))) {
+        this.actualUserId = parseInt(params['userId']);
+      }
+    });
+  }
+
+  private updateCurrentUser() {
+    this.selectedUser$ = this.store.pipe(select(getUserById(this.actualUserId)));
   }
 }
